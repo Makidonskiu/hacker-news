0f06e643548ab7d82f76adb18f6348f54a403f76
Makidonskiu <www.salamandr@gmail.com>
2023-06-09 07:26:15 +0300

    fixed comment nesting

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -7,20 +7,22 @@ import { Button } from 'antd';
 import './index.css';
 
 export const Comment = ({ comment, loadNestedComments }) => {
+  const formattedData = { __html: comment.text };
+
   const showComents = () => {
     loadNestedComments(comment)
   }
-
+  
   return (
     <li>
-      <p className='comment__text'>{comment.text}</p>
+      <p className='comment__text' dangerouslySetInnerHTML={formattedData} />
       <p className='comment__text'>Автор: {comment.by}</p>
       <p className='comment__text'>Дата: {new Date(comment.time * 1000).toLocaleString()}</p>
       {comment.kids && comment.kids.length > 0 && (
         <Button onClick={showComents}>Показать вложенные комментарии</Button>
       )}
       {comment.comments && comment.comments.length > 0 && (
-        <CommentsList comments={comment.comments} loadNestedComments={loadNestedComments} />
+        <CommentsList comments={comment.kids} loadNestedComments={loadNestedComments} />
       )}
     </li>
   );
--- src/pages/NewsPage/index.js
+++ src/pages/NewsPage/index.js
@@ -6,9 +6,9 @@ import { useDispatch, useSelector } from 'react-redux';
 // Redux
 import { fetchComments, fetchNewsItem } from '../../redux/slices/hackerNewsSlice';
 // Components
-import { CommentsList } from '../../components/CommentsList';
+import { Comment } from '../../components/Comment';
 // Antd
-import { Button } from 'antd';
+import { Button, List } from 'antd';
 // Css
 import './index.css';
 
@@ -18,20 +18,21 @@ export const NewsPage = () => {
   const newsItem = useSelector((state) => state.news.newsItem);
   const comments = useSelector((state) => state.news.comments);
   const url = useSelector((state) => state.news.url);
+  const status = useSelector((state) => state.news.status);
 
   React.useEffect(() => {
-    dispatch(fetchNewsItem(newsId)); 
+    dispatch(fetchNewsItem(newsId));
   }, [dispatch, newsId]);
 
   React.useEffect(() => {
     if (newsItem && newsItem.kids) {
-      dispatch(fetchComments(newsItem.kids)); 
+      dispatch(fetchComments(newsItem.kids || []));
     }
   }, [dispatch, newsItem]);
 
   const loadNestedComments = (comment) => {
     if (comment && comment.kids && comment.kids.length > 0) {
-      dispatch(fetchComments(comment.kids)); 
+      dispatch(fetchComments(comment.kids || []));
     }
   };
 
@@ -41,15 +42,41 @@ export const NewsPage = () => {
 
   return (
     <div>
-      <div className='news-page__button'><Link to="/hacker-news/">
-        <Button type="primary">Назад к списку новостей</Button>
-      </Link></div>
-      <h2 className='news-page__header'><a href={url} target='_blank' rel="noreferrer">{newsItem.title}</a></h2>
-      <p className='news-page__text'>Дата: {new Date(newsItem.time * 1000).toLocaleString()}</p>
-      <p className='news-page__text'>Автор: {newsItem.by}</p>
-      <p className='news-page__text'>Количество комментариев: {newsItem.kids ? newsItem.kids.length : 0}</p>
-      <Button onClick={() => loadNestedComments(newsItem)}>Обновить комментарии</Button>
-      {newsItem?.kids?.length > 0 && <CommentsList comments={comments} loadNestedComments={loadNestedComments} />}
+      <List
+        size="large"
+        header={
+          <>
+            <div className="news-page__button">
+              <Link to="/hacker-news/">
+                <Button type="primary">Назад к списку новостей</Button>
+              </Link>
+            </div>
+            <h2 className="news-page__header">
+              <a href={url} target="_blank" rel="noreferrer">
+                {newsItem.title}
+              </a>
+            </h2>
+            <p className="news-page__text">
+              Дата: {new Date(newsItem.time * 1000).toLocaleString()}
+            </p>
+            <p className="news-page__text">Автор: {newsItem.by}</p>
+            <p className="news-page__text">
+              Количество комментариев: {newsItem.kids ? newsItem.kids.length : 0}
+            </p>
+            <Button onClick={() => loadNestedComments(newsItem)}>Обновить комментарии</Button>
+          </>
+        }
+        footer={status === 'loading' && <h2>Loading...</h2>}
+        bordered
+        dataSource={comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} loadNestedComments={loadNestedComments} />
+      ))}
+        renderItem={(item) => (
+          <List.Item key={item.id}>
+            {newsItem?.kids?.length > 0 && <ul>{item}</ul>}
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
